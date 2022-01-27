e054467e9536330eec2ac8ff9019c72b21ba536c
Michael Ablassmeier <abi@grinser.de>
2022-01-27 19:42:15 +0100

    show some real progress

--- libqmpbackup/qmpcommon.py
+++ libqmpbackup/qmpcommon.py
@@ -200,7 +200,8 @@ class QmpCommon:
             else:
                 for job in status["return"]:
                     if job["device"] == device:
-                        log.info("Wrote Offset: %s of %s", job["offset"], job["len"])
+                        pr = [round(job["offset"]/job["len"]*100) if job["offset"] != 0 else 0]
+                        log.info("Wrote Offset: %s%% (%s of %s)", pr[0], job["offset"], job["len"])
             sleep(1)
 
     def do_qmp_backup(self, **kwargs):
