00ccdbac5c291be35e196cc0138371cb43a1f787
ma <ma@sep.de>
2024-01-25 13:25:15 +0100

    move code to flat file instead of class

--- libqmpbackup/vm.py
+++ libqmpbackup/vm.py
@@ -16,73 +16,67 @@ from collections import namedtuple
 log = logging.getLogger(__name__)
 
 
-class VMInfo:
-    """Get information about Vm blockdev"""
+def get_block_devices(blockinfo, excluded_disks, included_disks):
+    """Get a list of block devices that we can create a bitmap for,
+    currently we only get inserted qcow based images
+    """
+    BlockDev = namedtuple(
+        "BlockDev",
+        ["node", "format", "filename", "backing_image", "has_bitmap", "bitmaps"],
+    )
+    blockdevs = []
+    for device in blockinfo:
+        bitmaps = None
+        has_bitmap = False
+        backing_image = False
+        if "inserted" not in device:
+            log.debug("Ignoring non-inserted device: %s", device)
+            continue
 
-    @staticmethod
-    def get_block_devices(blockinfo, excluded_disks, included_disks):
-        """Get a list of block devices that we can create a bitmap for,
-        currently we only get inserted qcow based images
-        """
-        BlockDev = namedtuple(
-            "BlockDev",
-            ["node", "format", "filename", "backing_image", "has_bitmap", "bitmaps"],
-        )
-        blockdevs = []
-        for device in blockinfo:
-            bitmaps = None
-            has_bitmap = False
-            backing_image = False
-            if "inserted" not in device:
-                log.debug("Ignoring non-inserted device: %s", device)
-                continue
-
-            inserted = device["inserted"]
-            if inserted["drv"] == "raw":
-                log.warning(
-                    "Excluding device with raw format from backup: %s", device["device"]
-                )
-                continue
-
-            bitmaps = []
-            if "dirty-bitmaps" in inserted:
-                bitmaps = inserted["dirty-bitmaps"]
-
-            if "dirty-bitmaps" in device:
-                bitmaps = device["dirty-bitmaps"]
-
-            if len(bitmaps) > 0:
-                has_bitmap = True
-
-            try:
-                backing_image = inserted["image"]["backing-image"]
-                backing_image = True
-            except KeyError:
-                pass
-
-            if included_disks and not device["device"] in included_disks:
-                log.info(
-                    "Device not in included disk list, ignoring: %s", device["device"]
-                )
-                continue
-
-            if excluded_disks and device["device"] in excluded_disks:
-                logging.info("Excluding device from backup: %s", device["device"])
-                continue
-
-            log.debug("Adding device to device list: %s", device)
-            blockdevs.append(
-                BlockDev(
-                    device["device"],
-                    inserted["image"]["format"],
-                    inserted["image"]["filename"],
-                    backing_image,
-                    has_bitmap,
-                    bitmaps,
-                )
+        inserted = device["inserted"]
+        if inserted["drv"] == "raw":
+            log.warning(
+                "Excluding device with raw format from backup: %s", device["device"]
             )
+            continue
 
-        if len(blockdevs) == 0:
-            return None
+        bitmaps = []
+        if "dirty-bitmaps" in inserted:
+            bitmaps = inserted["dirty-bitmaps"]
 
-        return blockdevs
+        if "dirty-bitmaps" in device:
+            bitmaps = device["dirty-bitmaps"]
+
+        if len(bitmaps) > 0:
+            has_bitmap = True
+
+        try:
+            backing_image = inserted["image"]["backing-image"]
+            backing_image = True
+        except KeyError:
+            pass
+
+        if included_disks and not device["device"] in included_disks:
+            log.info("Device not in included disk list, ignoring: %s", device["device"])
+            continue
+
+        if excluded_disks and device["device"] in excluded_disks:
+            logging.info("Excluding device from backup: %s", device["device"])
+            continue
+
+        log.debug("Adding device to device list: %s", device)
+        blockdevs.append(
+            BlockDev(
+                device["device"],
+                inserted["image"]["format"],
+                inserted["image"]["filename"],
+                backing_image,
+                has_bitmap,
+                bitmaps,
+            )
+        )
+
+    if len(blockdevs) == 0:
+        return None
+
+    return blockdevs
