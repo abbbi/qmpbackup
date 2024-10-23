3303943bc249b59df2982b3e1b082d5c0db7e63c
ma <ma@sep.de>
2024-10-23 11:41:49 +0200

    ceph

--- libqmpbackup/vm.py
+++ libqmpbackup/vm.py
@@ -87,13 +87,21 @@ def get_block_devices(blockinfo, argv, excluded_disks, included_disks, uuid):
             try:
                 encoded_name = json.loads(filename[5:])
                 try:
-                    filename = encoded_name["file"]["next"]["filename"]
+                    driver = encoded_name["file"]["image"]["driver"]
+                    if driver == "rbd":
+                        log.info("Ceph device found, using image name")
+                        filename = encoded_name["file"]["image"]
                 except KeyError:
-                    log.warning(
-                        "Json encoded setting found but no filename property set for device: [%s]",
-                        device["device"],
-                    )
-                    continue
+                    pass
+                else:
+                    try:
+                        filename = encoded_name["file"]["next"]["filename"]
+                    except KeyError:
+                        log.warning(
+                            "Json encoded setting found but no filename property set for device: [%s]",
+                            device["device"],
+                        )
+                        continue
             except json.decoder.JSONDecodeError as errmsg:
                 log.warning(
                     "Unable to decode filename json for device [%s]: %s",
