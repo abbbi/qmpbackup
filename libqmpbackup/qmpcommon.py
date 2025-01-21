0dbaacf8ba039f138af8e5b590fedc2ae838d19b
Michael Ablassmeier <abi@grinser.de>
2025-01-21 21:19:53 +0100

    #18: fix backup issue: default_exception_handler: Task exception was never retrieved

--- libqmpbackup/qmpcommon.py
+++ libqmpbackup/qmpcommon.py
@@ -12,7 +12,7 @@ import os
 import logging
 from time import sleep
 import asyncio
-from qemu.qmp import EventListener
+from qemu.qmp import EventListener, qmp_client
 from libqmpbackup import fs
 
 
@@ -214,7 +214,7 @@ class QmpCommon:
         actions = self.prepare_transaction(argv, devices, uuid)
         with self.qmp.listen(listener):
             await self.qmp.execute("transaction", arguments={"actions": actions})
-            asyncio.create_task(self.progress(), name="progress")
+            _ = asyncio.create_task(self.progress(), name="progress")
             if qga is not False:
                 fs.thaw(qga)
             async for event in listener:
@@ -268,7 +268,10 @@ class QmpCommon:
         """Report progress for active block job"""
         while True:
             sleep(1)
-            jobs = await self.qmp.execute("query-block-jobs")
+            try:
+                jobs = await self.qmp.execute("query-block-jobs")
+            except qmp_client.ExecInterruptedError:
+                return
             if len(jobs) == 0:
                 return
             for job in jobs:
